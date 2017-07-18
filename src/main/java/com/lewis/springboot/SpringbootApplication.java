46bdf1c60a69c61b9a8793120480b1737162e92a
minghzhang007 <minghzhang@163.com>
2017-07-19 00:00:45 +0800

    Initial commit

---
+++ src/main/java/com/lewis/springboot/SpringbootApplication.java
@@ -0,0 +1,23 @@
+package com.lewis.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lewis0077 on 2017/7/18.
+ */
+@RestController
+@SpringBootApplication
+public class SpringbootApplication {
+
+    @RequestMapping("/")
+    String index(){
+        return "Hello ! Spring Boot !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class,args);
+    }
+}
